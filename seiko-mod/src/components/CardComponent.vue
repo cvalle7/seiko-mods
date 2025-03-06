02dee2e467860a38e1021c29a866f4011615cbe6
cvalle7 <cvb16@gmail.com>
2025-03-06 10:48:25 +0100

    fix front deleted

---
+++ seiko-mod/src/components/CardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.card-container {
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>

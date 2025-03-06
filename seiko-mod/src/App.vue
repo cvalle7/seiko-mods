462b436735a195d5aa4b4358276f4691afe8d76e
cvalle7 <cvb16@gmail.com>
2025-03-06 10:08:05 +0100

    init back

---
+++ seiko-mod/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <NavbarComponent/>
+  <router-view></router-view>
+</template>
+
+<script>
+import NavbarComponent from './components/NavbarComponent.vue';
+
+
+export default {
+  name: 'App',
+  components:{
+    NavbarComponent
+  }
+}
+</script>
+
+<style>
+
+html, body{
+  height: 100%;
+  margin: 0;
+  padding-top: 6vh;
+  overflow: auto;
+  scrollbar-width: none;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>

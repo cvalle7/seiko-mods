30e37316328b08b2f0231762e0f0340c4c7d4d85
cvalle7 <cvb16@gmail.com>
2025-03-07 10:08:58 +0100

    [WIP] - watch selected

--- seiko-mod/src/views/WatchView.vue
+++ seiko-mod/src/views/WatchView.vue
@@ -1,10 +1,34 @@
 <template>
     <div class="watch-container">
-        holas
+       <div class="watch-bar">
+        <ul class="list">
+            <li class="list-element" v-for="family in watchFamily" :key="family.id" :class="{'selected': (family.name == familySelected.name)}" @click="selectFamily(family)">
+                {{ family.name }}
+            </li>
+        </ul>
+       </div>
     </div>
 </template>
 
 <script setup>
+
+import {onMounted, ref} from "vue"
+import clockTypeService from '@/services/clockTypes.service'
+
+const watchFamily = ref([])
+const familySelected = ref('')
+
+const selectFamily = (family) => {
+    familySelected.value = family
+}
+
+onMounted(() => {
+    const getWatchFamily = async () => {
+        watchFamily.value = await clockTypeService.getClockTypes()
+    }
+
+    getWatchFamily()
+})
 </script>
 
 <style scoped>
@@ -15,5 +39,36 @@
     display: flex;
     align-items: center;
     justify-content: center;
+}
+
+.watch-bar{
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.list{
+    display: flex;
+    flex-direction: row;
+    gap: 3rem;
+    color: black;
+    font-size: 1.2rem;
+    border: 0;
+    list-style: none;
+}
+
+.list-element{
+    cursor: pointer;
+    transition: color 0.5s ease;
+}
+
+.list-element:hover{
+    color: #135A3A;
+}
+
+.selected{
+    list-style: disc;
+    color: #135A3A;
 }
 </style>
